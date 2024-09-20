1c225b0472220016d894dd07d2825d50f41fae44
jimmy.auris@outlook.com <jimmy.auris@outlook.com>
2024-09-19 23:51:19 -0500

    feat: cv jimmy auris v1.01

--- components/ui/section.tsx
+++ components/ui/section.tsx
@@ -1,13 +1,10 @@
 import { cn } from "@/lib/utils";
 import React from "react";
 
-export interface BadgeProps
-    extends React.HTMLAttributes<HTMLDivElement> {}
-
 export function Section({
     className,
     ...props
-}: BadgeProps) {
+}: React.HTMLAttributes<HTMLDivElement>) {
     return (
         <section
             className={cn(
